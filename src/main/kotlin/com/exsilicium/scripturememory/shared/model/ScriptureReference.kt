e157652a32a8df7a940f5707427dbe08d732805e
Ben Wicks <ben.wicks@buildertrend.com>
2017-09-22 16:08:08 -0500

    Remove dysfunctional comparable implementation from reference Make chapter non-nullable

--- src/main/kotlin/com/exsilicium/scripturememory/shared/model/ScriptureReference.kt
+++ src/main/kotlin/com/exsilicium/scripturememory/shared/model/ScriptureReference.kt
@@ -3,9 +3,9 @@ package com.exsilicium.scripturememory.shared.model
 data class ScriptureReference(
         // todo consider hierarchy: Book, or Book and Chapter(s), or Book and ch.(s) and verse(s)
         val book: Book,
-        val chapter: Int? = null, // todo permit chapter ranges like 2:9-3:2
+        val chapter: Int, // todo permit chapter ranges like 2:9-3:2
         val verseRanges: List<ClosedRange<Verse>> = listOf()
-): Comparable<ScriptureReference> {
+) {
     constructor(
             book: Book,
             chapter: Int,
@@ -17,25 +17,4 @@ data class ScriptureReference(
             chapter: Int,
             verse: Verse
     ) : this(book, chapter, listOf(VerseRange(verse)))
-
-    override fun compareTo(other: ScriptureReference): Int {
-        return book.compareTo(other.book).let {
-            when {
-                it != 0 -> it
-                chapter == null -> when {
-                    other.chapter == null -> 0
-                    else -> -1 // todo test this part
-                }
-                else -> when {
-                    other.chapter == null -> 1
-                    else -> chapter.compareTo(other.chapter).let {
-                        when {
-                            it != 0 -> it // todo fix below
-                            else -> verseRanges.compareTo(other.verseRanges)
-                        }
-                    }
-                }
-            }
-        }
-    }
 }
