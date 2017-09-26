b7787194c63aef3b5f32694c632431a069aa3481
Benjamin Wicks <benjaminlwicks@gmail.com>
2017-09-25 23:43:52 -0500

    Refactor Verse compareTo

--- src/main/kotlin/com/exsilicium/scripturememory/shared/model/Verse.kt
+++ src/main/kotlin/com/exsilicium/scripturememory/shared/model/Verse.kt
@@ -5,20 +5,13 @@ data class Verse(
         val number: Int,
         val part: Char? = null
 ) : Comparable<Verse> {
-    override fun compareTo(other: Verse) = chapter.compareTo(other.chapter).let {
-        when {
-            it != 0 -> it
-            else -> number.compareTo(other.number).let {
+    override fun compareTo(other: Verse): Int {
+        return chapter.compareTo(other.chapter).let {
+            if (it != 0) it else number.compareTo(other.number).let {
                 when {
                     it != 0 -> it
-                    part == null -> when {
-                        other.part == null -> 0
-                        else -> -1
-                    }
-                    else -> when {
-                        other.part == null -> 1
-                        else -> part.compareTo(other.part)
-                    }
+                    part == null -> if (other.part == null) 0 else -1
+                    else -> if (other.part == null) 1 else part.compareTo(other.part)
                 }
             }
         }
